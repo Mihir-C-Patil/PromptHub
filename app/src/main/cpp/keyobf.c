95a2b47a47613905458befbc10ad9e3db208e51c
Mihir-C-Patil <mihirpatilnz@gmail.com>
2025-05-20 13:32:37 +1200

    feat: Add NDK key obfuscation and Base64 utility
    This commit introduces NDK-based key obfuscation and a Base64 decoding utility.
    
    Key changes:
    - Added `keyobf.c` with JNI functions (`f0`, `f1`, `f2`) to store and obfuscate key parts.
    - Implemented a `base64.kt` utility object for decoding Base64 strings.
    - Updated the Android Gradle Plugin (AGP) version from "8.9.2" to "8.10.0" in `gradle/libs.versions.toml`.

---
+++ app/src/main/cpp/keyobf.c
@@ -0,0 +1,86 @@
+JNIEXPORT void JNICALL
+Java_com_example_prompthub_utils_KeyLoader_f0(JNIEnv
+*env,
+jobject thiz
+) {
+store_real_value(396, 52);
+store_real_value(532, 89);
+store_real_value(205, 106);
+store_real_value(130, 69);
+obfuscatedStorage[353] = 37;
+store_real_value(303, 90);
+obfuscatedStorage[120] = 32;
+obfuscatedStorage[218] = 32;
+store_real_value(28, 119);
+obfuscatedStorage[37] = 101;
+store_real_value(57, 106);
+store_real_value(599, 78);
+obfuscatedStorage[261] = 142;
+store_real_value(26, 69);
+store_real_value(83, 79);
+store_real_value(514, 68);
+store_real_value(507, 121);
+store_real_value(559, 100);
+store_real_value(579, 122);
+store_real_value(98, 81);
+store_real_value(464, 89);
+store_real_value(395, 106);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_prompthub_utils_KeyLoader_f1(JNIEnv
+*env,
+jobject thiz
+) {
+store_real_value(516, 78);
+store_real_value(278, 78);
+store_real_value(147, 86);
+store_real_value(347, 106);
+obfuscatedStorage[339] = 95;
+store_real_value(174, 50);
+store_real_value(248, 87);
+store_real_value(266, 48);
+store_real_value(452, 69);
+store_real_value(42, 65);
+obfuscatedStorage[393] = 91;
+obfuscatedStorage[349] = 71;
+store_real_value(358, 122);
+store_real_value(237, 109);
+obfuscatedStorage[35] = 64;
+store_real_value(2, 109);
+store_real_value(522, 78);
+store_real_value(311, 78);
+store_real_value(193, 109);
+obfuscatedStorage[312] = 96;
+store_real_value(449, 71);
+store_real_value(21, 69);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_prompthub_utils_KeyLoader_f2(JNIEnv
+*env,
+jobject thiz
+) {
+store_real_value(77, 121);
+store_real_value(506, 68);
+obfuscatedStorage[467] = 141;
+store_real_value(12, 122);
+store_real_value(344, 87);
+store_real_value(231, 81);
+obfuscatedStorage[359] = 138;
+store_real_value(546, 107);
+store_real_value(317, 90);
+store_real_value(0, 105);
+store_real_value(58, 90);
+store_real_value(257, 77);
+obfuscatedStorage[518] = 35;
+store_real_value(398, 69);
+store_real_value(162, 84);
+store_real_value(508, 74);
+store_real_value(551, 71);
+obfuscatedStorage[141] = 74;
+store_real_value(580, 78);
+store_real_value(586, 78);
+store_real_value(529, 82);
+obfuscatedStorage[369] = 34;
+}
